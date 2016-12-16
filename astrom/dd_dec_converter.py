Some bugs fixed. Trying to improve rotation algorithm

astrom/dd_dec_converter.py
@@ -1,5 +1,4 @@
-from __future__ import print_function
-from __future__ import division
+from __future__ import print_function, division
 import numpy as np
 
 
@@ -8,6 +7,7 @@ def hmsToDeg(hms,sep=":"):
     if len(hmssplit) != 3:
         raise ValueError('Unknown format of RA. Maybe wrong seperator?,', ra)
     sign = np.sign(hmssplit[0])
+    if sign == 0: sign = 1
     deg = hmssplit[0]*15. + sign* hmssplit[1]/4. + sign*hmssplit[2] / 240.
     
     return deg
@@ -18,6 +18,7 @@ def dmsToDeg(hms,sep=":"):
     if len(hmssplit) != 3:
         raise ValueError('Unknown format of RA. Maybe wrong seperator?,', ra)
     sign =  np.sign(hmssplit[0])
+    if sign == 0: sign = 1
     deg = hmssplit[0] + sign * hmssplit[1]/60. + sign * hmssplit[2] / 3600.
     
     return deg

astrom/misc.py
@@ -1,10 +1,10 @@
-from __future__ import print_function
-from __future__ import division
+from __future__ import print_function,division
 import numpy as np
 import matplotlib.pyplot as plt
 import math
 from parameters import *
 import dd_dec_converter as dc
+from shutil import copy2
 from astropy.coordinates import SkyCoord
 import pandas as pd
 import misc
@@ -76,21 +76,46 @@ def rotate_coords(x,y,angle,rad = False,center=[0.,0.]):
     y += center[1]
     return x,y
     
-def get_catalogue_data(fn_source,mjd_source,mjd_obs=None,verbose=True,plot=True):
+def get_catalogue_data(fn_source,mjd_source,coord_head,mjd_obs=None,verbose=True,plot=True):
     #Reads the positions in RA and DEC from the catalogue given. if mjd is given it calculates
     #the current position based on the pm found in that catalogue
     
     #read in the coords and convert them
-    if verbose: print('Converting coordinates. Assuming deg and mas/yr given in',fn_source,pm_ra,pm_dec)
+    if verbose: print('Converting coordinates. Assuming hmsdms and mas/yr given in',fn_source,pm_ra,pm_dec)
     source = pd.read_csv(fn_source,delimiter =',')
+    for numeric in [mag,pm_ra,pm_dec]:#convert to floats
+        source[numeric] = pd.to_numeric(source[numeric],errors='coerce')
+    rem_coord = []
+
+    if len(str(source[ra][00]).strip().split(' ')) == 3: #assuming hms/dms
+        if verbose: print('Converting coordinates. Assuming hms/dms and mas/yr given in',fn_source,pm_ra,pm_dec)
+        coord_fmt = 'hmsdms'
+    else: #assuming deg
+        if verbose: print('Converting coordinates. Assuming deg and mas/yr given in',fn_source,pm_ra,pm_dec) 
+        coord_fmt = 'deg'
     for ii in xrange(len(source[ra])):
-        source[ra][ii] = dc.hmsToDeg(source[ra][ii].strip(), sep=' ')
-        source[dec][ii]= dc.dmsToDeg(source[dec][ii].strip(),sep=' ')
+        if coord_fmt == 'hmsdms':
+            coord_dum = SkyCoord(dc.hmsToDeg(source[ra][ii].strip(), sep=' '), \
+                                 dc.dmsToDeg(source[dec][ii].strip(),sep=' '), \
+                                 frame='icrs',unit='deg')
+        elif coord_fmt == 'deg':
+            coord_dum = SkyCoord(source[ra][ii],source[dec][ii],frame='icrs',unit='deg')
+        else: raise ValueError('Unknown coordinate format. Use hmsdms with space separation or deg')
+
+        if coord_head.separation(coord_dum).arcsec <= query_around_head_coord:
+            source[ra][ii] = coord_dum.ra.deg
+            source[dec][ii]= coord_dum.dec.deg
+        else: 
+            rem_coord.append(ii)
+                    
+    source = source.drop(rem_coord)
+    source.reset_index(inplace=True)
+    
 
     #check for validity of mjd_obs and set to source date (zero pm) if none is given
     if mjd_obs != None:
-        if mjd_obs <= 57400 or mjd_obs >= 65000:
-            raise ValueError('mjd of ',mjd,' smaller than 57400! This should not be the case for ISPY')
+        if mjd_obs <= 57300 or mjd_obs >= 65000:
+            raise ValueError('mjd of ',mjd_obs,' smaller than 57300! This should not be the case for ISPY')
     else:
         mjd_obs = mjd_source
     
@@ -111,15 +136,15 @@ def get_catalogue_data(fn_source,mjd_source,mjd_obs=None,verbose=True,plot=True)
     dec_now= np.array(dec_now)
     source['ra_now'] = ra_now
     source['dec_now']= dec_now
-    source['ra_now_rot'],source['dec_now_rot'] = misc.rotate_coords(ra_now,dec_now,-rot_init,\
-                                                                    center=[np.mean(ra_now),np.mean(dec_now)])
+#    source['ra_now_rot'],source['dec_now_rot'] = misc.rotate_coords(ra_now,dec_now,-rot_init,\
+#                                                    center=[np.mean(ra_now),np.mean(dec_now)])
     #plot the result
     if plot:
         plt.xlabel('RA [deg]')
         plt.ylabel('DEC [deg]')
         plt.scatter(source[ra],source[dec],label = 'Orig. Pos.',color='blue')
         plt.scatter(source['ra_now'],source['dec_now'], color='red',label='With PM',alpha = 0.3)
-        plt.scatter(source['ra_now_rot'],source['dec_now_rot'], color='k',label='With PM and rotated',alpha = 0.3)
+       # plt.scatter(source['ra_now_rot'],source['dec_now_rot'], color='k',label='With PM and rotated',alpha = 0.3)
         plt.legend()
         plt.savefig(dir_out+'catalogue_stars.svg')
         plt.close('all')
@@ -142,12 +167,18 @@ def weighted_avg_and_std(values, weights):
 
 def get_headerparams(header,verbose=True):
     targetname = header['OBJECT'].lower()
-    if 'trapezium' in targetname:
-        target = 'trapezium'
-    elif '47tuc' in targetname:
-        target = '47tuc'
-    else:
-        raise ValueError('Target ',targetname,' unknown. Please provide info for it.')
+    target = None
+    coord_head = SkyCoord(header['CRVAL1'],header['CRVAL2'],'icrs',unit='deg')
+    while target == None:
+        if ('trapezium' in targetname) or (coord_head.separation(coord_trapez).arcsec < 30) :
+            target = 'trapezium'
+        elif ('47tuc' in targetname) \
+             or (coord_head.separation(coord_47tuc_sphere).arcsec < 30)\
+             or (coord_head.separation(coord_47tuc_wolfgang).arcsec < 30):
+            target = '47tuc'
+        else:
+            print('ATTENTION!!! Target <',targetname,'> unknown. Please type trapezium or 47tuc .')
+            targetname = str(raw_input())
     if 'apo_165' in header['Apodizer'].lower():
         agpm=True
     else:
@@ -158,11 +189,11 @@ def get_headerparams(header,verbose=True):
     if verbose: print('Found the following configuration:\n AGPM: %s \n target: %s \n mjd_obs: %s' \
                       %(agpm,target,mjd_obs))
     
-    return target,mjd_obs,agpm
+    return target,mjd_obs,agpm,coord_head
 
-def get_filenames(target,agpm,verbose=True):
+def get_catfiles(target,agpm,dir_cat,dir_temp,verbose=True):
     try: fn_sextr_med
-    except NameError: fn_sextr_med = dir_cat+target+'_median.sex'
+    except NameError: fn_sextr_med = dir_cat+target+'_med.sex'
     try: fn_sextr_sgl
     except NameError: fn_sextr_sgl = dir_cat+target+'_single.sex'
     try: fn_source
@@ -171,5 +202,11 @@ def get_filenames(target,agpm,verbose=True):
     except NameError:
         if target == 'trapezium':
             mjd_source = 55850.
-    if verbose: print('Using the following parameterfiles: \n sextractor median:%s \n sextractor single: %s \n source catalog: %s \n mjd of source cat: %s' %(fn_sextr_med,fn_sextr_sgl,fn_source,mjd_source))
+        if target == '47tuc':
+            mjd_source = 52369.5
+    if verbose: print('Using and copying the following parameterfiles: \n sextractor median:%s \n sextractor single: %s \n source catalog: %s \n mjd of source cat: %s' %(fn_sextr_med,fn_sextr_sgl,fn_source,mjd_source))
+    for fn in [fn_sextr_med,fn_sextr_sgl,fn_source]:
+        copy2(fn,dir_temp+os.path.basename(fn))
+        fn = os.path.basename(fn)
+    copy2(dir_cat+'sex.param',dir_temp+'sex.param')
     return mjd_source,fn_sextr_med,fn_sextr_sgl,fn_source

astrom/plots.py
@@ -1,5 +1,4 @@
-from __future__ import print_function
-from __future__ import division
+from __future__ import print_function, division
 import os
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
@@ -11,54 +10,55 @@ import misc
 from parameters import *
 
 
-def make_artificial_map(source,data_objects,verbose=True,plot=True):
+def make_artificial_map(source,data_objects,verbose=True,plot=True,use_mags =True):
     '''creates an artificial map with the sources in it'''
     print('Matching the sources...')
     offset = 3*fwhm
     degtopix = 60*60*1000/pxscl_init
-    ra_min = min(source['ra_now_rot'])
-    dec_min= min(source['dec_now_rot'])
-    x_dim = int(round((max(source['ra_now_rot'])-ra_min) *  degtopix + 2*offset))
-    y_dim = int(round((max(source['dec_now_rot'])-dec_min)* degtopix + 2*offset))
+    ra_min = min(source['ra_now'])
+    dec_min= min(source['dec_now'])
+    x_dim = int(round((max(source['ra_now'])-ra_min) *np.cos(dec_min*np.pi/180.)*  degtopix + 2*offset))
+    y_dim = int(round((max(source['dec_now'])-dec_min)* degtopix + 2*offset))
     source_map = np.zeros((y_dim,x_dim))
     gauss =misc.makeGaussian(3*fwhm,fwhm = fwhm,center = None)
+    #gauss = np.array([[1,]])
     
     #put the gaussian to the coordinates. Scale with mag in logspace. Real fluxes would overkill.
     x_im =[]
     y_im =[]
     for ii in range(len(source['ra_now_rot'])):
-        x = offset + (source['ra_now_rot'][ii] - ra_min) * degtopix
+        x = offset + (source['ra_now_rot'][ii] - ra_min) * np.cos(source['dec_now_rot'][ii]*np.pi/180.)* degtopix
         y = offset + (source['dec_now_rot'][ii]- dec_min)* degtopix
         x_im.append(x)
         y_im.append(y)
-        if np.isfinite(source[mag][ii]):
-            source_map[int(y-np.floor(0.5*gauss.shape[0])):int(y+np.ceil(0.5*gauss.shape[0])),
-                       int(x-np.floor(0.5*gauss.shape[1])):int(x+np.ceil(0.5*gauss.shape[1]))] +=\
-                    gauss*(max(source[mag]+1-source[mag][ii]))
+        if use_mags and np.isfinite(source[mag][ii]):
+                source_map[int(y-np.floor(0.5*gauss.shape[0])):int(y+np.ceil(0.5*gauss.shape[0])),
+                           int(x-np.floor(0.5*gauss.shape[1])):int(x+np.ceil(0.5*gauss.shape[1]))] +=\
+                gauss*np.sqrt(10.**( (max(source[mag])-source[mag][ii]) / 2.5 ))
+                #gauss*(max(source[mag]+1-source[mag][ii]))
         else:
             source_map[int(y-np.floor(0.5*gauss.shape[0])):int(y+np.ceil(0.5*gauss.shape[0])),
                        int(x-np.floor(0.5*gauss.shape[1])):int(x+np.ceil(0.5*gauss.shape[1]))] +=\
-                    gauss* 1
+                    gauss*1
     source['x_im_rot']=x_im
     source['y_im_rot']=y_im
     #flip in right direction to align with image and rotate it
     source_map = np.fliplr(source_map)
     source['x_im_rot'] =source_map.shape[1] -source['x_im_rot']
-    corr = signal.fftconvolve(source_map,np.sqrt(data_objects[::-1,::-1]),mode='same')
+    corr = signal.fftconvolve(source_map,(data_objects[::-1,::-1]**0.15),mode='same')
     y_corr,x_corr = np.unravel_index(np.argmax(corr),corr.shape)
-
     #plot the results
     if verbose: print('Plotting the results')
     if plot:
         if verbose:  print('Plotting the results')
         fig, (ax_source,ax_data,ax_corr) = plt.subplots(1,3)
-        ax_source.imshow(source_map)    
+        ax_source.imshow(source_map[::-1,:])    
         ax_source.plot(x_corr,y_corr,'ro')
         ax_source.add_patch(
             mpatches.Rectangle((x_corr-data_objects.shape[1]/2.,y_corr-data_objects.shape[0]/2.),\
                                data_objects.shape[1],data_objects.shape[0],alpha=0.3,fc='r'))
         ax_source.set_title('Catalog objects')
-        ax_data.imshow(np.sqrt(data_objects))
+        ax_data.imshow(data_objects**0.15)
         ax_data.set_xlim((0,data_objects.shape[1]))
         ax_data.set_ylim((0,data_objects.shape[0]))
         ax_data.set_title('Sextracted median')
@@ -75,12 +75,11 @@ def make_artificial_map(source,data_objects,verbose=True,plot=True):
             ax_data.plot(x_data[ii],y_data[ii],'ro',alpha= 0.3, ms= 4)
             ax_data.annotate(source[star_id][ii],xy=(x_data[ii],y_data[ii]),size=6)
     if plot:
-        fig.savefig(dir_out+'correlated_image.svg')
+        fig.savefig(dir_out+'correlated_image.pdf')
         plt.close('all')
     source['x_data_rot'] = x_data
     source['y_data_rot'] = y_data
-   
-
+    
     return source
 
 

astrom/psf_fitting.py
@@ -20,7 +20,7 @@ import subprocess
 
 import ipdb
 
-def make_psf(data_cube,sex_coords,n_rms=2,conv_gauss=True,keepfr=0.7,verbose=True):
+def make_psf(data_cube,sex_coords,n_rms=2,conv_gauss=True,keepfr=0.7,fn_out='psf_cube',verbose=True):
     '''Makes a psf out of the stars in the images found by fitting and median combining them.n_rms
     gives the image size in rms of the ellipse parameter of the brightest star found by sextractor 
     to which the images are cropped.'''
@@ -30,10 +30,11 @@ def make_psf(data_cube,sex_coords,n_rms=2,conv_gauss=True,keepfr=0.7,verbose=Tru
     n_stars = 0
     for i_image in range(n_images):
         n_stars += len(sex_coords[i_image])
+        sex_coords[i_image].reset_index(inplace=True)
 
     i_max = sex_coords[0]['mag_auto'].idxmin()
     #get rms (fwhm/2) for 4micron at 8m telescope
-    size = 1.44*4e-6 /8.4 /math.pi*360*3600*1000/pxscl_init/2
+    size = 1.44*4e-6 /8.4 /math.pi*360*3600*1000/pxscl_init/2.
 #uncomment for sextractor values
 #    size = int(round(n_rms* math.sqrt(math.pow(sex_coords[0]['rms_A_image'][i_max],2) +\
 #                                      math.pow(sex_coords[0]['rms_B_image'][i_max],2))))
@@ -120,8 +121,8 @@ def make_psf(data_cube,sex_coords,n_rms=2,conv_gauss=True,keepfr=0.7,verbose=Tru
     
     second_med = np.median(selected_stars,axis=0)
 
-    fits.writeto(dir_temp+'psf_cube_w_median.fits',np.concatenate((data_stars,[first_med]),axis=0))
-    fits.writeto(dir_temp+'psf_cube_selected_w_median.fits',np.concatenate((selected_stars,[second_med]),axis=0))
+    fits.writeto(dir_temp+fn_out+'_w_median.fits',np.concatenate((data_stars,[first_med]),axis=0))
+    fits.writeto(dir_temp+fn_out+'_selected_w_median.fits',np.concatenate((selected_stars,[second_med]),axis=0))
     return second_med
             
 def refine_fit(data_cube,data_psf,sex_coords,conv_gauss = True,verbose=True):

astrom/sextract.py
@@ -20,7 +20,7 @@ def coordinates(data_cube,header_cube,target,fn_sextr_sgl,fn_sextr_med,med=False
     If med=True, doing the same with different output names 
     (_med instead of _{image_number}) and returning also the med image'''
 
-    os.chdir(dir_out)
+    os.chdir(dir_temp)
     FNULL = open(os.devnull,'w') #suppress output
     if not med:
         n_images = data_cube.shape[0]
@@ -32,7 +32,6 @@ def coordinates(data_cube,header_cube,target,fn_sextr_sgl,fn_sextr_med,med=False
                   '-catalog_name',dir_temp+target+'_'+'{:03}'.format(kk)+'.sex'],stdout=FNULL,
                  stderr=subprocess.STDOUT)
             call(['rm','temp.fits'])
-        os.chdir(dir_temp)
         all_objects = np.full(data_cube.shape, np.nan)
         sex_coords = [None] * n_images
         for kk in range(n_images):
@@ -54,9 +53,9 @@ def coordinates(data_cube,header_cube,target,fn_sextr_sgl,fn_sextr_med,med=False
               '-checkimage_name',dir_temp+target+'_objects_med.fits',
               '-catalog_name',dir_temp+target+'_med.sex'],stdout=FNULL,
              stderr=subprocess.STDOUT)
+
         call(['rm','temp.fits'])
         #now that we have all data in files, read it in
-        os.chdir(dir_temp)
         all_objects = np.full(data_cube.shape, np.nan)
         sex_coords = [None]
         all_objects = fits.getdata(dir_temp+target+'_objects_med.fits')
@@ -65,6 +64,7 @@ def coordinates(data_cube,header_cube,target,fn_sextr_sgl,fn_sextr_med,med=False
                                 names=('mag_auto','x_image','y_image','rms_A_image','rms_B_image'),
                                 dtype={'x_image':np.float64,'y_image':np.float64},
                                 skip_blank_lines = True)
+        sex_coords = sex_coords.sort_values(by='mag_auto').reset_index(drop=True)
         #subtract 1 as sextractor starts at (1,1) and not (0,0)
         sex_coords['x_image'] -= 1
         sex_coords['y_image'] -= 1

platescale.py
@@ -46,9 +46,11 @@ def do(verbose=True,plot=True,delete_old=True):
     astrom.make_dirs([dir_out,dir_temp],delete_old=delete_old,verbose=verbose)
     filename_cube, data_cube, header_cube =  astrom.read_fits(dir_data,verbose=verbose)
     #determine some parameters from the fits files
-    target,mjd_obs,agpm = misc.get_headerparams(header_cube[0],verbose=verbose)
+    target,mjd_obs,agpm,coord_head = misc.get_headerparams(header_cube[0],verbose=verbose)
     #determine more (standard) parameters as filenames (if None in parameters file)
-    mjd_source,fn_sextr_med,fn_sextr_sgl,fn_source = misc.get_filenames(target,agpm,verbose=verbose)
+    mjd_source,fn_sextr_med,fn_sextr_sgl,fn_source = misc.get_catfiles(target,agpm,
+                                                                        dir_cat,dir_temp,
+                                                                        verbose=verbose)
         
     data_med = np.median(data_cube,axis=0)
     
@@ -67,7 +69,7 @@ def do(verbose=True,plot=True,delete_old=True):
     #quick sanity check
     if header_cube[-1]['MJD-OBS'] - mjd_obs > 1:
         raise ValueError('Observations not from the same day:',mjd_obs, header_cube[-1]['MJD-OBS'])
-    source_cat  = astrom.misc.get_catalogue_data(fn_source,mjd_source,mjd_obs=mjd_obs,
+    source_cat  = astrom.misc.get_catalogue_data(fn_source,mjd_source,coord_head,mjd_obs=mjd_obs,
                                                  verbose=verbose, plot=plot)
 
     ###################################################
@@ -76,7 +78,7 @@ def do(verbose=True,plot=True,delete_old=True):
     #####sextractor####################################
     ###################################################
     source_cat  = astrom.plots.make_artificial_map(source_cat,data_objects_med,
-                                                   verbose=verbose,plot=plot)
+                                                   verbose=verbose,plot=plot,use_mags=True)
     
     #Only use the sources found in the median image further. Find them
     source_med,ignored_med = astrom.analysis.connect_median_sources(source_cat,median_sources,
@@ -90,9 +92,11 @@ def do(verbose=True,plot=True,delete_old=True):
 
     sex_coords  = sextract.coordinates(data_cube,header_cube,target,fn_sextr_sgl,fn_sextr_med,
                                        med=False,verbose=verbose)
-    
+    psf_med = psf_fitting.make_psf(np.array([data_med]),[source_med],fn_out = 'med_cube',keepfr=0.9)
+    source_med = psf_fitting.refine_fit(np.array([data_med]),psf_med,[source_med],
+                                        conv_gauss=conv_gauss,verbose=verbose)[0]
     #make a reference psf to use to center the stars
-    psf_stars = psf_fitting.make_psf(data_cube,sex_coords, n_rms = 2, conv_gauss=conv_gauss, keepfr = keepfr)
+    psf_stars = psf_fitting.make_psf(data_cube,sex_coords, n_rms = 2, conv_gauss=conv_gauss, keepfr = keepfr,fn_out='psf_cube')
     #find all targets via a ccmap and the psf just created)
     #sex_coords_ccmap = psf_fitting.find_via_ccmap(data_cube,psf_stars,target)
     #refine the stellar positions with the new psf. old ones in x/y_image_sex column
@@ -124,7 +128,6 @@ def do(verbose=True,plot=True,delete_old=True):
     #in results these will be grouped.
     astrometry = astrom.calc_platescale.single_matches(data_cube,sex_coords,nr_ign_im,
                                                        verbose=verbose,plot=plot)
-
     ################################################################
     #now combine all measurements that have the same start and end##
     ################################################################